b2eff9278b0e69ee003c0e713be0eb32ba446770
DmitrySevostenkevich <passattikk@gmail.com>
2023-09-24 20:44:46 +0300

    initial commit

---
+++ src/main/java/org/example/lesson1/Test1.java
@@ -0,0 +1,11 @@
+package org.example.lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the first lesson!!!");
+        System.out.print("Helo...");
+
+        System.out.println("Меня зовут");
+        System.out.println("Заур");
+    }
+}
