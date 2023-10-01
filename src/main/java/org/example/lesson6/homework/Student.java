6fd3a3bf8121fa869a9910c73a9b2bf8a08d3dd3
DmitrySevostenkevich <passattikk@gmail.com>
2023-10-01 11:03:39 +0300

    Домашка 6 урока

--- src/main/java/org/example/lesson6/homework/Student.java
+++ src/main/java/org/example/lesson6/homework/Student.java
@@ -1,6 +1,12 @@
 package org.example.lesson6.homework;
+/*
+Измените класс Student что бы он имел 3 конструктора .
+1 принимает все параметры.
+2-ой: id, name,sureName,course
+3-ий: не принимает значений
+Создайте в классе StudentTest 3 обьекта с помощью разных конструкторов.
 
-
+ */
 public class Student {
     String sureName;
     String name;
